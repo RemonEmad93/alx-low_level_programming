6357ab832e3e75e91a55d7c8935969962ae38d09
Remon Emad <remonemad658@gmail.com>
2023-02-21 14:18:54 -0800

    modify task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,15 +3,17 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-void times_table(void) {
+void times_table(void)
+{
 	int i, j, res;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			res = i * j;
-			putchar('0' + (res / 10));
-			putchar('0' + (res % 10));
+			putchar((res / 10));
+			putchar((res % 10));
 			putchar(',');
 		}
 		putchar('\n');
