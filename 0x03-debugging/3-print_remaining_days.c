1d8ae41e476de204f0db01c15f9e67811655baa4
Remon Emad <remonemad658@gmail.com>
2023-02-23 05:42:48 -0800

    modify task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,28 +13,27 @@
 void print_remaining_days(int month, int day, int year)
 {
 	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int total_days = 0;
-	int remaining_days;
+	int day_of_year = 0;
+	int i;
 
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
 		days_in_month[2] = 29;
 	}
 
-	for (int i = 1; i < month; i++)
+	for (i = 1; i < month; i++)
 	{
-		total_days += days_in_month[i];
+		day_of_year += days_in_month[i];
 	}
-	total_days += day;
+	day_of_year += day;
 
-	if (days_in_month[2] == 29 && month >= 3)
+	if (day_of_year > 365 && !((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
 	{
-		remaining_days = 366 - total_days;
-	} else
-	{
-		remaining_days = 365 - total_days;
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 	}
-
-	printf("Day of the year: %d\n", total_days);
-	printf("Remaining days: %d\n", remaining_days);
+	else
+	{
+		printf("Day of the year: %d\n", day_of_year);
+		printf("Remaining days: %d\n", 365 - day_of_year);
+	}
 }
