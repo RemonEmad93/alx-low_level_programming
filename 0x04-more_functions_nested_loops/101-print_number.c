cf5565581f97b1a117c78fb80f0d137699f9a89f
Remon Emad <remonemad658@gmail.com>
2023-02-23 15:07:08 -0800

    done task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	int divisor = 1, i, digit, negative = 0;
+
+	if (n < 0)
+	{
+		putchar('-');
+		negative = 1;
+	}
+
+	for (i = 0; n / divisor > 9 || n / divisor < -9; i++)
+		divisor *= 10;
+
+	for (; divisor != 0; divisor /= 10)
+	{	
+		digit = n / divisor;
+		if (negative)
+			putchar('0' - digit);
+		else
+			putchar('0' + digit);
+
+		n %= divisor;
+	}
+}
