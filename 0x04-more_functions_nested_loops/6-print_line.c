a2304640bd8a1352e9071c9452f20b9fbb2198e4
Remon Emad <remonemad658@gmail.com>
2023-02-23 16:20:16 -0800

    modify task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,17 @@
  */
 void print_line(int n)
 {
-    if (n > 0)
-    {
-        int i;
+	if (n > 0)
+	{
+		int i;
 
-        for (i = 0; i < n; i++)
-            putchar('_');
+	for (i = 0; i < n; i++)
+		putchar('_');
 
-        putchar('\n');
-    }
-    else
-    {
-        putchar('\n');
-    }
+		putchar('\n');
+	}
+	else
+	{
+		putchar('\n');
+	}
 }
