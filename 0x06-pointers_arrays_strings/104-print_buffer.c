bbb8efd9d8b0ade6425923c7157d4e3aad90e9d2
Remon Emad <remonemad658@gmail.com>
2023-03-01 13:46:22 -0800

    modify task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -18,7 +18,6 @@ void print_buffer(char *b, int size)
 		/* print the position of the first byte of the line in hexadecimal */
 		printf("%08x: ", i);
 
-		/* print the hexadecimal content (2 chars) of the buffer, 2 bytes at a time, separated by a space */
 		for (j = 0; j < 10; j++)
 		{
 			if (j + i >= size) /* end of buffer */
@@ -36,16 +35,14 @@ void print_buffer(char *b, int size)
 			if (j + i >= size) /* end of buffer */
 				break;
 
-			/* if the byte is a printable character, print the letter, if not, print . */
 			if (*(b + i + j) >= 32 && *(b + i + j) <= 126)
 				printf("%c", *(b + i + j));
 			else
 				printf(".");
 		}
 
-		printf("\n"); /* end of line */	
+		printf("\n"); /* end of line */
 	}
-
 	if (size <= 0) /* output a new line if size is 0 or less */
 		printf("\n");
 }
