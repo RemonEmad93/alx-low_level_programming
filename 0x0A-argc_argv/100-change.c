c58f8ee6b428ccf65b3dfd02cb8a07a194e2f67d
Remon Emad <remonemad658@gmail.com>
2023-03-10 14:04:20 -0800

    modify some tasks

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,49 +2,60 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
+ * min_coins - calculates the minimum number of coins to make change
+ * for an amount of money
+ * @cents: the amount of money in cents
  *
- * Description: Prints the minimum number of coins to make change for an
- * amount of money in cents using 25, 10, 5, 2, and 1 cent coins.
- *
- * @argc: The number of arguments passed to the program
- * @argv: An array of strings containing the arguments passed to the program
+ * Return: the minimum number of coins
+ */
+int min_coins(int cents)
+{
+	int coins = 0;
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else
+			cents -= 1;
+
+		coins++;
+	}
+
+	return (coins);
+}
+
+/**
+ * main - the entry point of the program
+ * @argc: the number of arguments passed
+ * @argv: an array of pointers to the arguments
  *
  * Return: 0 on success, 1 on failure
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	int cents, num_coins;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+		cents = 0;
 
-	int coins = 0;
+	num_coins = min_coins(cents);
 
-	coins += cents / 25;
-	cents %= 25;
-
-	coins += cents / 10;
-	cents %= 10;
-
-	coins += cents / 5;
-	cents %= 5;
-
-	coins += cents / 2;
-	cents %= 2;
-
-	coins += cents;
-
-	printf("%d\n", coins);
+	printf("%d\n", num_coins);
 
 	return (0);
 }
