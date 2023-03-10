d35e90b8eef03d82c6156b9f2740709ad35917ec
Remon Emad <remonemad658@gmail.com>
2023-03-10 13:21:58 -0800

    done task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	/* Print the name of the program */
+	printf("%s\n", argv[0]);
+
+	/* Return success */
+	return (0);
+}
