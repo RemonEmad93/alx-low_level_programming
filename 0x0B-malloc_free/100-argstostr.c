906a1d29402d5529647040ddcf1b6de690a923d8
Remon Emad <remonemad658@gmail.com>
2023-03-13 13:36:46 -0700

    done all tasks

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all arguments of a program
+ * @ac: the number of arguments
+ * @av: an array of pointers to the arguments
+ *
+ * Return: a pointer to a new string, or NULL if it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k, len;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * strtow - splits a string into words
+ * @str: the string to split
+ *
+ * Return: a pointer to an array of strings (words), or NULL if it fails
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int i, j, k, len, count;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	count = 0;
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
+
+	words = malloc(sizeof(char *) * (count + 1));
+	if (words == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ')
+			continue;
+		len = 0;
+		for (j = i; str[j] && str[j] != ' '; j++)
+			len++;
+        words[k] = malloc(sizeof(char) * (len + 1));
+        if (words[k] == NULL)
+        {
+            for (i = 0; i < k; i++)
+                free(words[i]);
+            free(words);
+            return (NULL);
+        }
+        for (j = 0; j < len; j++)
+            words[k][j] = str[i + j];
+        words[k][j] = '\0';
+        k++;
+        i += len;
+    }
+    words[k] = NULL;
+
+    return (words);
+}
