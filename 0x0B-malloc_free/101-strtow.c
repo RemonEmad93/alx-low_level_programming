aec7a8ba137287e71061696a67eff8b5dc44dd65
Remon Emad <remonemad658@gmail.com>
2023-03-13 14:00:15 -0700

    modify task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,54 +2,121 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+#include <stdlib.h>
+
+int count_words(char *str);
+char *get_word(char *str);
+int get_word_length(char *str);
+
 /**
  * strtow - splits a string into words
- * @str: the string to split
+ * @str: string to split
  *
- * Return: a pointer to an array of strings (words), or NULL if it fails
+ * Return: pointer to an array of strings (words)
  */
 char **strtow(char *str)
 {
-	char **words;
-	int i, j, k, len, count;
+    char **words;
+    int i, j, word_count = 0, word_length;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
+    if (str == NULL || *str == '\0')
+        return (NULL);
 
-	count = 0;
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-			count++;
-	}
+    word_count = count_words(str);
+    if (word_count == 0)
+        return (NULL);
 
-	words = malloc(sizeof(char *) * (count + 1));
-	if (words == NULL)
-		return (NULL);
+    words = malloc((word_count + 1) * sizeof(char *));
+    if (words == NULL)
+        return (NULL);
 
-	k = 0;
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] == ' ')
-			continue;
-		len = 0;
-		for (j = i; str[j] && str[j] != ' '; j++)
-			len++;
-		words[k] = malloc(sizeof(char) * (len + 1));
-		if (words[k] == NULL)
-		{
-			for (i = 0; i < k; i++)
-				free(words[i]);
-			free(words);
-			return (NULL);
-		}
-		for (j = 0; j < len; j++)
-			words[k][j] = str[i + j];
-		words[k][j] = '\0';
-		k++;
-		i += len;
-	}
-	words[k] = NULL;
+    for (i = 0, j = 0; i < word_count; i++)
+    {
+        while (*str == ' ')
+            str++;
 
-	return (words);
+        word_length = get_word_length(str);
+        words[i] = get_word(str);
+        if (words[i] == NULL)
+        {
+            for (j = 0; j < i; j++)
+                free(words[j]);
+            free(words);
+            return (NULL);
+        }
+        str += word_length;
+    }
+
+    words[i] = NULL;
+    return (words);
+}
+
+/**
+ * count_words - counts the number of words in a string
+ * @str: string to count words in
+ *
+ * Return: number of words
+ */
+int count_words(char *str)
+{
+    int count = 0;
+
+    while (*str)
+    {
+        if (*str != ' ')
+        {
+            count++;
+            while (*str != ' ' && *str != '\0')
+                str++;
+        }
+        else
+        {
+            str++;
+        }
+    }
+
+    return (count);
+}
+
+/**
+ * get_word - extracts a single word from a string
+ * @str: string to extract word from
+ *
+ * Return: pointer to the extracted word
+ */
+char *get_word(char *str)
+{
+    char *word;
+    int i, length;
+
+    length = get_word_length(str);
+
+    word = malloc((length + 1) * sizeof(char));
+    if (word == NULL)
+        return (NULL);
+
+    for (i = 0; i < length; i++)
+        word[i] = str[i];
+
+    word[i] = '\0';
+    return (word);
+}
+
+/**
+ * get_word_length - gets the length of a single word in a string
+ * @str: string to get word length from
+ *
+ * Return: length of the word
+ */
+int get_word_length(char *str)
+{
+    int length = 0;
+
+    while (*str != ' ' && *str != '\0')
+    {
+        length++;
+        str++;
+    }
+
+    return (length);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,3 +6,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int count_words(char *str);
+char *get_word(char *str);
+int get_word_length(char *str);
