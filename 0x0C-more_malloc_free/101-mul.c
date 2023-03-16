4d8c7ee82b0586122ffca2a49745c79e615b4603
Remon Emad <remonemad658@gmail.com>
2023-03-16 15:26:49 -0700

    modify some tasks

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,7 +11,7 @@
  */
 int _isdigit(int c)
 {
-    return (c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
 
 /**
@@ -22,12 +22,12 @@ int _isdigit(int c)
  */
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (*s++)
-        len++;
+	while (*s++)
+		len++;
 
-    return (len);
+	return (len);
 }
 
 /**
@@ -36,8 +36,8 @@ int _strlen(char *s)
  */
 void _puts(char *s)
 {
-    while (*s)
-        putchar(*s++);
+	while (*s)
+		putchar(*s++);
 }
 
 /**
@@ -45,9 +45,9 @@ void _puts(char *s)
  */
 void print_error(void)
 {
-    _puts("Error");
-    putchar('\n');
-    exit(98);
+	_puts("Error");
+	putchar('\n');
+	exit(98);
 }
 
 /**
@@ -59,55 +59,21 @@ void print_error(void)
  */
 int main(int argc, char **argv)
 {
-    int i, j, len1, len2, carry, *result;
-    char *num1, *num2;
+	char *num1, *num2;
 
-    if (argc != 3)
-        print_error();
+	if (argc != 3 || !_isnumber(argv[1]) || !_isnumber(argv[2]))
+		print_error();
 
-    num1 = argv[1];
-    num2 = argv[2];
+	num1 = argv[1];
+	num2 = argv[2];
 
-    len1 = _strlen(num1);
-    len2 = _strlen(num2);
+	char *product = multiply(num1, num2);
 
-    for (i = 0; i < len1; i++)
-        if (!_isdigit(num1[i]))
-            print_error();
+	if (product == NULL)
+		return (1);
 
-    for (i = 0; i < len2; i++)
-        if (!_isdigit(num2[i]))
-            print_error();
+	printf("%s\n", product);
+	free(product);
 
-    result = calloc(len1 + len2, sizeof(int));
-    if (result == NULL)
-        return (1);
-
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            carry += (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1];
-            result[i + j + 1] = carry % 10;
-            carry /= 10;
-        }
-        result[i] += carry;
-    }
-
-    i = 0;
-    while (i < len1 + len2 && result[i] == 0)
-        i++;
-
-    if (i == len1 + len2)
-        putchar('0');
-    else
-    {
-        for (; i < len1 + len2; i++)
-            putchar(result[i] + '0');
-    }
-    putchar('\n');
-
-    free(result);
-    return (0);
+	return (0);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,21 +11,20 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-        char *ptr;
-        unsigned int i, total_size;
+	char *ptr;
+	unsigned int i, total_size;
 
-        if (nmemb == 0 || size == 0)
-                return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-        total_size = nmemb * size;
-        ptr = malloc(total_size);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
 
-        if (ptr == NULL)
-                return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 
-        for (i = 0; i < total_size; i++)
-                ptr[i] = 0;
+	for (i = 0; i < total_size; i++)
+		ptr[i] = 0;
 
-        return (ptr);
+	return (ptr);
 }
-
