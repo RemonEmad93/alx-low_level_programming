627aaf7c0183abdfd96805331438b83240184e15
Remon Emad <remonemad658@gmail.com>
2023-03-16 16:22:35 -0700

    modify somw tasks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,33 +9,34 @@
  * @old_size: The size, in bytes, of the allocated space for ptr
  * @new_size: The new size, in bytes, of the new memory block
  *
- * Return: Pointer to the newly allocated memory block, or NULL if an error occurs
+ * Return: Pointer to the  allocated memory block, or NULL if an error occurs
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-        void *new_ptr;
+	void *new_ptr;
 
-        if (new_size == 0 && ptr != NULL) {
-                free(ptr);
-                return (NULL);
-        }
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-        if (new_size == old_size)
-                return (ptr);
+	if (new_size == old_size)
+		return (ptr);
 
-        if (ptr == NULL)
-                return (malloc(new_size));
+	if (ptr == NULL)
+		return (malloc(new_size));
 
-        new_ptr = malloc(new_size);
+	new_ptr = malloc(new_size);
 
-        if (new_ptr == NULL)
-                return (NULL);
+	if (new_ptr == NULL)
+		return (NULL);
 
-        if (new_size > old_size)
-                memcpy(new_ptr, ptr, old_size);
-        else
-                memcpy(new_ptr, ptr, new_size);
+	if (new_size > old_size)
+		memcpy(new_ptr, ptr, old_size);
+	else
+		memcpy(new_ptr, ptr, new_size);
 
-        free(ptr);
-        return (new_ptr);
+	free(ptr);
+	return (new_ptr);
 }
