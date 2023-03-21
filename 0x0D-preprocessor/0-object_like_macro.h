5c836df7366faab029c424dd75acf65779277ec6
Remon Emad <remonemad658@gmail.com>
2023-03-20 17:34:01 -0700

    done task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 0-object_like_macro
-#define 0-object_like_macro
+#ifndef 0-object_like_macro.h
+#define 0-object_like_macro.h
 
 #define SIZE 1024
 
-#endif 0-object_like_macro
+#endif 0-object_like_macro.h
