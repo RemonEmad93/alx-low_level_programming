8ec85dfd70e88ac125e69ee7eb672c4e5c44970a
Remon Emad <remonemad658@gmail.com>
2023-03-30 09:50:14 -0700

    modify some tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,32 +13,34 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	list_t *new_node = malloc(sizeof(list_t));
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->len = strlen(str);
-    new_node->next = NULL;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        list_t *current = *head;
-        while (current->next != NULL)
-            current = current->next;
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-        current->next = new_node;
-    }
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		list_t *current = *head;
 
-    return (new_node);
+		while (current->next != NULL)
+			current = current->next;
+
+		current->next = new_node;
+	}
+
+	return (new_node);
 }
