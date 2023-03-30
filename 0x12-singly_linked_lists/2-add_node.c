37ec8f0c36acd271a3943d6f2c772e41252a6d45
Remon Emad <remonemad658@gmail.com>
2023-03-30 09:57:06 -0700

    modify some tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -9,20 +11,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	list_t *new_node = malloc(sizeof(list_t));
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
 
-    return (new_node);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
