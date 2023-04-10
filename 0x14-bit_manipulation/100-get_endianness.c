3bdf5d177eafe5e0c878e8ceb48e2f281ff531a2
Remon Emad <remonemad658@gmail.com>
2023-04-10 12:33:56 -0700

    modify some tasks

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,12 +3,12 @@
 /**
  * get_endianness - checks the endianness of the system
  *
- * Return: 0 if big endian, 1 if little endian
+ * Return: 0 if big endian and 1 if little endian
  */
 int get_endianness(void)
 {
 	unsigned int x = 1;
 	char *c = (char *) &x;
 
-	return (*c) ? 1 : 0;
+	return ((*c) ? 1 : 0);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits needed to flip to get from one number to another
+ * flip_bits - return num of bits needed to flip to get from one num to another
  * @n: the first number
  * @m: the second number
  *
