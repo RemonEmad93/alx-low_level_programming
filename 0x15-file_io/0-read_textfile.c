d60809d13c26d3877f21a57abd0508d5307dce29
Remon Emad <remonemad658@gmail.com>
2023-04-10 12:51:23 -0700

    done all tasks

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: the actual number of letters it could read and print,
+ *         or 0 if the file can not be opened or read, or if filename is NULL,
+ *         or if write fails or does not write the expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd, num_read;
+    char *buf;
+
+    if (!filename)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    buf = malloc(sizeof(char) * letters);
+    if (!buf)
+    {
+        close(fd);
+        return (0);
+    }
+
+    num_read = read(fd, buf, letters);
+    if (num_read == -1)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
+
+    if (write(STDOUT_FILENO, buf, num_read) != num_read)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
+
+    free(buf);
+    close(fd);
+
+    return (num_read);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of pointers to arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, ret;
+	ssize_t read_count, write_count;
+	char buf[BUF_SIZE];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	do {
+		read_count = read(fd_from, buf, BUF_SIZE);
+		if (read_count == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+
+		write_count = write(fd_to, buf, read_count);
+		if (write_count == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	} while (read_count > 0);
+
+	ret = close(fd_from);
+	if (ret == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+
+	ret = close(fd_to);
+	if (ret == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+	return (0);
+}
+
